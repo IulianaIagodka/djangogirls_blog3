abc5d0e1db1c339e43dd1bf7eead42dd1b00ebac
IulianaIagodka <iuliana.iagodka@gmail.com>
2019-11-20 15:02:51 +0200

    new files

--- blog/views.py
+++ blog/views.py
@@ -5,14 +5,17 @@ from .forms import PostForm
 from django.shortcuts import render, get_object_or_404
 from django.shortcuts import redirect
 
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -26,6 +29,7 @@ def post_new(request):
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+
 
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
